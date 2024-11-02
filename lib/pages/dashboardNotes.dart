e3f0a48db51130267afeead1d8e38f674d138c4c
Laysa Santiago Alfredo <laysatb78@gmail.com>
2024-11-02 16:27:29 -0300

    feat: Adicionado estilo na dashboard do aplicativo
    - Implementado classes de estilos para a font-size e para as cores;
    - Implementado button Flutuante;

---
+++ lib/pages/dashboardNotes.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:notes_flutter/constants/app_styles.dart';
+
+class DashboardNotes extends StatefulWidget {
+  const DashboardNotes({super.key});
+
+  @override
+  State<DashboardNotes> createState() => _DashboardNotesState();
+}
+
+class _DashboardNotesState extends State<DashboardNotes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+        const Text(
+          "Notes",
+          style: TextStyle(
+            fontSize: FontSizeStyle.title,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: SizedBox(
+              width: 50,
+              height: 50,
+              child: TextButton(
+                onPressed: () => {},
+                style: TextButton.styleFrom(
+                  backgroundColor: ColorStyle.lightSkyBlue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14.0),
+                  ),
+                  padding: EdgeInsets.zero,
+                ),
+                child: const Icon(
+                  CupertinoIcons.search,
+                  color: ColorStyle.primaryBlue,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: const Center(),
+      floatingActionButton:
+        SizedBox(
+          width: 75,
+          height: 75,
+          child: FloatingActionButton(
+            backgroundColor: ColorStyle.primaryBlack,
+            onPressed: () {  },
+            shape: const CircleBorder(),
+            child: const Icon(
+              CupertinoIcons.plus,
+              size: 32,
+              color: ColorStyle.lightGray,
+            ),
+          ),
+      )
+    );
+  }
+}

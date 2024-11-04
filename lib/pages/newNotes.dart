25a4b1a6cc493451cf596425b234bd859fd92ddf
Laysa Santiago Alfredo <laysatb78@gmail.com>
2024-11-04 08:47:08 -0300

    feat: Adicionado tela para criaçáo de novas notas
    - iniciano a implementaçao das novas notas

--- lib/pages/newNotes.dart
+++ lib/pages/newNotes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_flutter/models/note.dart';
 
 class NewNotes extends StatefulWidget {
   const NewNotes({super.key});
@@ -8,11 +9,27 @@ class NewNotes extends StatefulWidget {
 }
 
 class _NewNotesState extends State<NewNotes> {
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _contentController = TextEditingController();
+
+  Future<void> _saveNote() async {
+    if (_titleController.text.isNotEmpty && _contentController.text.isNotEmpty) {
+      Note newNotes = Note(
+        title: _titleController.text,
+        content: _contentController.text,
+        createDate: DateTime.now(),
+        updateDate: DateTime.now(),
+      );
+      //Add no Firebase
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Test"),
+        child: Text("LAYSA LINDONA"),
       ),
     );
   }
